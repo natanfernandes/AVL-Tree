94ab15c6a7f128acc6efe437f9a6babb00a09fce
Natanael <natan-fernandes1@hotmail.com>
2018-11-29 22:39:42 -0300

    [A

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ class Node():
         self.val = val
         self.esquerda = None
         self.direita = None
-        self.sucessor = None
-        self.antecessor = None
 
 class Tree():
     def __init__(self):
@@ -17,6 +15,26 @@ class Tree():
             return self.altura
         else: 
             return 0
+
+    def sucessor(self,node):    #retorna o sucessor, o menor valor a esquerda
+        node = node.direita.node  
+        if node != None: 
+            while node.esquerda != None:    #procura a esquerda ate a arvore ficar vazia
+                if node.esquerda.node == None: 
+                    return node 
+                else: 
+                    node = node.esquerda.node  
+        return node 
+
+    def antecessor(self, node): #retorna o antecessor, o maior valor a direita
+        node = node.esquerda.node 
+        if node != None: 
+            while node.direita != None:   #procura a direita ate a arvore ficar vazia
+                if node.direita.node == None: 
+                    return node 
+                else: 
+                    node = node.direita.node  
+        return node 
 
     def calcFatorBalanceamento(self, recursivo = True):
         #fb = alturaDireita - alturaEsquerda    
@@ -77,39 +95,47 @@ class Tree():
 
     
     def rotacionarDireita(self):
-        aux1 = self.node #guardando o no atual
-        aux2 = self.node.esquerda.node #guardando o no a esquerda
-        aux3 = aux2.direita.node #guardando o no a direita do que esta a esquerda do atual
+        #aux1 = raiz antiga
+        #aux2 = filho a esquerda da raiz antiga
+        #aux3 = filho a direita do filho a esquerda
+        aux1 = self.node 
+        aux2 = self.node.esquerda.node
+        aux3 = aux2.direita.node 
         
-        self.node = aux2
-        aux2.direita.node = aux1
-        aux1.esquerda.node = aux3
+        self.node = aux2 #nova raiz
+        aux2.direita.node = aux1  #raiz original vira filho a direita da nova raiz
+        aux1.esquerda.node = aux3   #vira filho da esquerda do filho a direita
 
 
     def rotacionarEsquerda(self):
+        #aux1 = raiz antiga
+        #aux2 = filho a direita da raiz antiga
+        #aux3 = filho a esquerda do filho a direita
         aux1 = self.node 
         aux2 = self.node.direita.node 
         aux3 = aux2.esquerda.node 
         
-        self.node = aux2 
-        aux2.esquerda.node = aux1 
-        aux1.direita.node = aux3
+        self.node = aux2 #nova raiz
+        aux2.esquerda.node = aux1 #raiz original vira filho a esquerda da nova raiz
+        aux1.direita.node = aux3 #vira filho da direita do filho a esquerda
 
     def inserir(self,val):
         tree = self.node
         
-        newnode = Node(val)
+        newNode = Node(val)
         
         if tree == None:
-            self.node = newnode 
+            self.node = newNode 
             self.node.esquerda = Tree() 
             self.node.direita = Tree()
             print("inseriu : ", val )
         
         elif val < tree.val: 
-            self.node.esquerda.inserir(val)
+            #se valor que tentar inserir for menor, ele chama o metodo dnv para a esquerda
+            self.node.esquerda.inserir(val) 
             
         elif val > tree.val: 
+            #se valor que tentar inserir for maior, ele chama o metodo dnv para a direita
             self.node.direita.inserir(val)
         
         else: 
@@ -118,16 +144,72 @@ class Tree():
         self.rebalancear() 
     
     def deletar(self,val):
-        pass
+        if self.node != None: 
+            if self.node.val == val: 
+                if self.node.esquerda.node == None and self.node.direita.node == None:
+                    self.node = None 
+               
+                # deleta todos os filhos da sub-arvore
+                # caso ache a esquerda ou a direita 
+                elif self.node.esquerda.node == None: 
+                    self.node = self.node.direita.node
+                elif self.node.direita.node == None: 
+                    self.node = self.node.esquerda.node
+                
+                # caso tenha filho tanto a esquerda quanto a direita
+                # ele acha um sucessor e faz a troca entre os valores 
+                # e chama o metodo para excluir o filho a direita
+                else:  
+                    aux = self.sucessor(self.node)
+                    if aux != None:   
+                        self.node.val = aux.val 
+                        self.node.direita.deletar(aux.val)
+                    
+                self.rebalancear()
+                return  
+            elif val < self.node.val: 
+                self.node.esquerda.deletar(val)  
+            elif val > self.node.val: 
+                self.node.direita.deletar(val)
+                        
+            self.rebalancear()
+        else: 
+            return 
 
     def buscar(self,val):
-        pass
+        if self.node != None:
+            if self.node.val == val:
+                print('Achado o valor : ',val)
+            elif val < self.node.val: 
+                self.node.esquerda.buscar(val)  
+            elif val > self.node.val: 
+                self.node.direita.buscar(val)
+        else:
+            print('Nao esta na arvore')
+
+    def print(self,level=0, pref=''):
+        self.calcTamanho()
+        if(level == 0):
+            pref = "(Raiz)"
+       
+        self.calcFatorBalanceamento()
+        if(self.node != None): 
+            print ('Nivel : ',str(level), pref,' Valor do No : ', self.node.val, "[ Altura = " + str(self.altura) + " : FB = " + str(self.fator_balanceamento) + "]", 'Folha' if self.e_folha() else ' ')   
+            if self.node.esquerda != None: 
+                self.node.esquerda.print(level + 1, '<')
+            if self.node.esquerda != None:
+                self.node.direita.print(level + 1, '>')
 
 if __name__ == "__main__": 
     tree = Tree()
     tree.inserir(1)
     tree.inserir(5)
     tree.inserir(89)
+    tree.inserir(96)
+    tree.inserir(2)
     tree.inserir(4)
-    tree.inserir(1)
-    print(tree.node)
+    
+    tree.print()
+    tree.deletar(96)
+    tree.print()
+    tree.buscar(4)
